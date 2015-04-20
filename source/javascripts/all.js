929b0072e9353a0b3fa71edd51fbf792555632fc
Phil Sturgeon <me@philsturgeon.uk>
2015-04-20 17:55:22 -0400

    Added Facebook tracking.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -85,3 +85,20 @@ var Avgrund = (function(){
 	}
 
 })();
+
+
+// Facebook Pixel
+(function() {
+var _fbq = window._fbq || (window._fbq = []);
+if (!_fbq.loaded) {
+	var fbds = document.createElement('script');
+	fbds.async = true;
+	fbds.src = '//connect.facebook.net/en_US/fbds.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(fbds, s);
+	_fbq.loaded = true;
+}
+_fbq.push(['addPixelId', '1568071020112356']);
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', 'PixelInitialized', {}]);
