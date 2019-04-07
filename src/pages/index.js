57bfad952486aeac1a611996e4105d3995de8dd9
Mike Bifulco <mbifulco@live.com>
2019-04-07 10:35:01 -0400

    adjusting layout so not everything is green

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,17 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
+import classes from './Home.module.css';
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+
+    <section className={classes.books}>
+      <div className={classes.container}>
+        <h1>{`Surviving Other People's APIs`}</h1>
+      </div>
+    </section>
   </Layout>
 );
 
