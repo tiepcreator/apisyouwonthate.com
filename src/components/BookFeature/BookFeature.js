2b85e686cdebd2499bd4114f33dab3cddcf09673
Mike Bifulco <mbifulco@live.com>
2019-04-11 17:14:49 -0400

    simplify this guy to use internal component

--- src/components/BookFeature/BookFeature.js
+++ src/components/BookFeature/BookFeature.js
@@ -1,30 +1,13 @@
 import React from 'react';
-
-import { graphql, useStaticQuery } from 'gatsby';
-import Img from 'gatsby-image';
-
 import Media from 'react-bootstrap/Media';
 
-const BookFeature = ({ relativePath }) => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     file(relativePath: { eq: "build-apis-you-wont-hate.jpg" }) {
-  //       childImageSharp {
-  //         # Specify the image processing specifications right in the query.
-  //         # Makes it trivial to update as your page's design changes.
-  //         fixed(width: 125, height: 125) {
-  //           ...GatsbyImageSharpFixed
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
+import Img from '../image';
 
-  // if (!relativePath) return null;
+const BookFeature = ({ relativePath }) => {
   return (
     <Media>
       <Media.Body>
-        {/* <Img fixed={data.file.childImageSharp.fixed} />; */}
+        <Img src={relativePath} />;
       </Media.Body>
     </Media>
   );
