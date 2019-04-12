30fa2617393b364aabbda1998dde788fdd1c1483
Mike Bifulco <mbifulco@live.com>
2019-04-11 22:40:10 -0400

    use custom button

--- src/components/BookFeature/BookFeature.js
+++ src/components/BookFeature/BookFeature.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { Button, Col, Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import { Link } from 'gatsby';
 
 import Img from '../image';
+import { Button } from '../Button';
 import classes from './BookFeature.module.css';
 
 const BookFeature = ({ book }) => {
+  const bookUrl = `books/${book.frontmatter.slug}`;
   return (
     <Row>
       <Col>
         <div className={classes.metadata}>
           <h2>
-            <Link
-              className={classes.title}
-              to={`books/${book.frontmatter.slug}`}
-            >
+            <Link className={classes.title} to={bookUrl}>
               {book.frontmatter.title}
             </Link>
           </h2>
           <h3 className={classes.subtitle}>{book.frontmatter.subtitle}</h3>
           <p className={classes.description}>{book.frontmatter.description}</p>
-          <Button variant="secondary">Find out more</Button>
+          <Button to={bookUrl}>Find out more</Button>
         </div>
       </Col>
       <Col xs={4}>
