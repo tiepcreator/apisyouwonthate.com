e0a9564e0743f4f54c32fe785da44f9b61581614
Mike Bifulco <mbifulco@live.com>
2019-04-11 22:48:25 -0400

    add about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 
 // boostrap stuff
-import Container from 'react-bootstrap/Container';
+import { Col, Container, Row } from 'react-bootstrap';
 
 import { BookFeature } from '../components/BookFeature';
 import Layout from '../components/layout';
@@ -31,6 +31,32 @@ const IndexPage = ({ data }) => {
           })}
         </Container>
       </div>
+
+      <Container className={classes.about}>
+        <Row>
+          <Col xl={{ span: 8, offset: 2 }}>
+            <h2>About this Community</h2>
+            <p>
+              API development is a topic very close to Phils heart. APIs You
+              Won't Hate started out as a book, with Phil pouring everything API
+              related he knew, all the problems he faced, all the design
+              decisions he wish he thought about earlier.
+            </p>
+            <p>
+              Since the book, Phil has continued to learn and grow, thanks to
+              new experiences, and new conversations with really smart people.
+              Learning never stops, and the Slack channel that grew from the
+              book subscribers and their friends has become home to the largest
+              API chat group on the internet.
+            </p>
+            <p>
+              APIs You Won't Hate is dedicated to learning, writing, sharing
+              ideas and bettering understanding of API practices. Together we
+              can erradicate APIs we hate.
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </Layout>
   );
 };
