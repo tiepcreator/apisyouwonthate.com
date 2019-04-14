a6ce915d32813301cf0d234506f54ea62676c4e4
Mike Bifulco <mbifulco@live.com>
2019-04-14 11:06:37 -0400

    configure post display

--- src/templates/blog/Post.js
+++ src/templates/blog/Post.js
@@ -11,6 +11,7 @@ import { Container, Col, Row } from 'react-bootstrap';
 import Layout from '../../components/layout';
 
 import classes from './Post.module.css';
+import AuthorDisplay from '../../components/AuthorDisplay/AuthorDisplay';
 
 const Post = ({ data, pageContext }) => {
   const post = data.mdx;
@@ -20,11 +21,17 @@ const Post = ({ data, pageContext }) => {
       <Helmet>
         <title>{post.frontmatter.title}</title>
       </Helmet>
-      <Container className={classes.container}>
+      <Container className={classes.post}>
         <Row>
           <Col lg={{ span: 10, offset: 1 }} xl={{ span: 8, offset: 2 }}>
-            <h2>{post.frontmatter.title}</h2>
-            <h3>{post.frontmatter.author}</h3>
+            <div className={classes.metadata}>
+              <h2 className={classes.postTitle}>{post.frontmatter.title}</h2>
+              <AuthorDisplay
+                name={post.frontmatter.author}
+                date={post.frontmatter.date}
+                readTimeInMinutes={post.timeToRead}
+              />
+            </div>
             <MDXRenderer>{post.code.body}</MDXRenderer>
           </Col>
         </Row>
@@ -46,8 +53,11 @@ export const query = graphql`
         body
         scope
       }
+      timeToRead
       frontmatter {
         title
+        author
+        date
       }
     }
   }
