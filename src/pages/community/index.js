ace6361584c4d8b25c0c1ce8ad8abbe4ebfad72c
Mike Bifulco <mbifulco@live.com>
2019-04-19 14:45:13 -0400

    patreon on community page

--- src/pages/community/index.js
+++ src/pages/community/index.js
@@ -16,27 +16,41 @@ const CommunityPage = () => (
       <Container>
         <Row>
           <Col>
-            <h1>Community</h1>
-            <p>
-              Get your API, REST and HTTP questions answered by almost 2,000
-              people! We're the largest API-centric Slack channel around.
-            </p>
-
-            <p>
-              Joining is as easy as going to our{' '}
-              <OutboundLink
-                href="http://slack.apisyouwonthate.com/"
-                target="_blank"
-                rel="noreferrer noopener"
-              >
-                Slack signup form
-              </OutboundLink>
-              , and entering your email address.
-            </p>
-
-            <Image src="slack.png" alt="Our slack community" />
+            <h1>Patreon</h1>
+            <p>{"We'd love it if you supported us on Patreon."}</p>
+            <OutboundLink to="https://www.patreon.com/bePatron?u=4477635">
+              <Image
+                src="patreon-button.png"
+                className={classes.patreonButton}
+              />
+            </OutboundLink>
           </Col>
         </Row>
+        <div className={classes.slack}>
+          <Row>
+            <Col>
+              <h1>Slack</h1>
+              <p>
+                Get your API, REST and HTTP questions answered by almost 2,000
+                people! We're the largest API-centric Slack channel around.
+              </p>
+
+              <p>
+                Joining is as easy as going to our{' '}
+                <OutboundLink
+                  href="http://slack.apisyouwonthate.com/"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  Slack signup form
+                </OutboundLink>
+                , and entering your email address.
+              </p>
+
+              <Image src="slack.png" alt="Our slack community" />
+            </Col>
+          </Row>
+        </div>
       </Container>
     </div>
   </Layout>
