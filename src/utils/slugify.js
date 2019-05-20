ca13c8b70ec0e60b965ee34950c7f20667febdc6
Mike Bifulco <mbifulco@live.com>
2019-05-20 10:10:06 -0400

    convert `/` into `-` instead of removing

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,12 +1,15 @@
 // using require syntax so this works in gatsby-node.js as well 😞
 const slugifyLib = require('slugify');
 
+// set up slugify to convert `/` to `-` instead of removing it
+slugifyLib.extend({ '/': '-' });
+
 const slugify = input => {
   return slugifyLib(input, {
     lower: true,
-    replacement: '',
+    replacement: '-',
     // this regex contains characters to remove from slugs.  Add any that may be missing!
-    remove: /[…?/*+~.,()'"!:@]/g,
+    remove: /[…?*+~.,()'"!:@]/g,
   });
 };
 
