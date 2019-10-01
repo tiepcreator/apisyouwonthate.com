588e9620669796b7eb45349094f6737a664e7a0f
Mike Bifulco <mbifulco@live.com>
2019-10-01 12:53:47 -0500

    add AMA page for taking podcast questions/suggestions (#279)

---
+++ src/pages/ama/index.js
@@ -0,0 +1,153 @@
+import React, { useState } from 'react';
+import { graphql, Link } from 'gatsby';
+import { Button, Container, Col, Form, Row } from 'react-bootstrap';
+
+import { Layout } from '../../components';
+import classes from './AMA.module.css';
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+};
+
+const AMAPage = () => {
+  const [formData, setFormData] = useState({});
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...formData }),
+    })
+      .then(() => setSubmitted(true))
+      .catch(error => alert(error));
+  };
+
+  const handleChange = e =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+
+  return (
+    <Layout>
+      <Container className={classes.container}>
+        {submitted ? (
+          <>
+            <Row>
+              <Col>
+                <h1>Thank you!</h1>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <p>
+                  We'll do our best to answer your question as quickly as
+                  possible. Make sure to subscribe to the{' '}
+                  <Link to="/podcast">podcast</Link>, too. Have a great day!
+                </p>
+              </Col>
+            </Row>
+          </>
+        ) : (
+          <>
+            <Row>
+              <Col>
+                <h1>Got a question or comment?</h1>
+                <p>
+                  We'd love to help you out! Leave us a question or comment
+                  below, and we'll do our best to answer it via twitter, or on
+                  the Podcast.
+                </p>
+              </Col>
+            </Row>
+
+            <Form
+              name="contact"
+              data-netlify="true"
+              netlify-honeypot="bot-field"
+              className={classes.form}
+              onSubmit={handleSubmit}
+            >
+              <Form.Group controlId="formBasicEmail">
+                <Form.Group controlId="formBasicName">
+                  <Form.Label>Your name</Form.Label>
+                  <Form.Control
+                    onChange={handleChange}
+                    name="contact-name"
+                    type="text"
+                    placeholder="Josephine Doe"
+                  />
+                </Form.Group>
+
+                <Form.Label>Your contact information</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="contact-info"
+                  type="text"
+                  placeholder="@irreverentmike or hello@apisyouwonthate.com"
+                />
+                <Form.Text className="text-muted">
+                  We'll use this to tag you in a response. Leave blank to ask
+                  anonymously
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group controlId="formBasicComment">
+                <Form.Label>Your question or comment</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  name="comment"
+                  as="textarea"
+                  rows={3}
+                  placeholder="What the heck is an API?"
+                />
+              </Form.Group>
+
+              <Form.Group className={classes.hidden}>
+                <Form.Label>Don’t fill this out if you're human:</Form.Label>
+                <Form.Control
+                  onChange={handleChange}
+                  type="text"
+                  placeholder="Away, botspam"
+                  name="bot-field"
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </>
+        )}
+      </Container>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    podcasts: allAnchorEpisode {
+      nodes {
+        id
+        title
+        pubDate
+        link
+        content
+        contentSnippet
+        isoDate
+        itunes {
+          image
+          episode
+          season
+          duration
+        }
+      }
+    }
+  }
+`;
+
+export default AMAPage;
