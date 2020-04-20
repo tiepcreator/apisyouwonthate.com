102e16e5978d85b9be76f9b651c3c42e270408c0
matt trask <mjftrask@gmail.com>
2020-04-20 08:37:07 -0500

    update text, added new fields

--- src/pages/jobs/index.js
+++ src/pages/jobs/index.js
@@ -23,8 +23,9 @@ const JobsPage = ({ data }) => {
             <div className={classes.heading}>
               <h2>API Jobs</h2>
               <p>We put this together to help people find jobs in the API Space. Whether you want to do API Design, Implementation, Testing, Open Source work etc, this will serve as a long lasting directory of jobs in the API Space vetted by professionals.</p>
-              <p>For companies today, we are posting jobs for free so that way we can get as many jobs in front of developers as we can.</p>
+              <p>For companies today, we are posting jobs for free so that way we can get as many jobs in front of developers as we can with everything in the world going on. There are tons of amazing developers looking for work, and we want to keep it as easy as possible.</p>
               <p>To add a job, make a pull request to <a href="https://github.com/apisyouwonthate/apisyouwonthate.com">the project repository</a> and use <a href="https://github.com/apisyouwonthate/apisyouwonthate.com/blob/master/src/content/jobs/primitive-php-developer.mdx">this</a> as a template to filling out your posting.</p>
+              <p>If you would rather, you can email your job to <a href="mailto:mjtrask@gmail.com?subject=New API Job!">Matt</a> or <a href="mailto:mbifulco@live.com?subject=New API Job!">Mike</a>.</p>
             </div>
           </Row>
         </Col>
