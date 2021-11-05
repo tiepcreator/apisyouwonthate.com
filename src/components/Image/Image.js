bfa59c0f8654ff3b214157ee17d59c2a74280883
Mike Bifulco <mbifulco@live.com>
2021-11-05 13:39:37 -0400

    need dynamic images

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useStaticQuery, graphql } from 'gatsby';
-import { StaticImage } from 'gatsby-plugin-image';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 export const Image = ({ src, alt, ...rest }) => {
-  // const { allImageSharp } = useStaticQuery(graphql`
-  //   query {
-  //     allImageSharp {
-  //       edges {
-  //         node {
-  //           fluid {
-  //             originalName
-  //           }
-  //           gatsbyImageData(layout: FULL_WIDTH)
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
-  // const image = allImageSharp.edges.find(
-  //   (edge) => edge.node.fluid.originalName === src
-  // );
+  const { allImageSharp } = useStaticQuery(graphql`
+    query {
+      allImageSharp {
+        edges {
+          node {
+            fluid {
+              originalName
+            }
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+  `);
+  const image = allImageSharp.edges.find(
+    (edge) => edge.node.fluid.originalName === src
+  );
 
-  // if (!image) {
-  //   return null;
-  // }
-  // return <GatsbyImage image={image.node.gatsbyImageData} alt={alt} {...rest} />;
-  return <StaticImage src={src} />;
+  if (!image) {
+    return null;
+  }
+  return <GatsbyImage image={image.node.gatsbyImageData} alt={alt} {...rest} />;
 };
 
 Image.propTypes = {
