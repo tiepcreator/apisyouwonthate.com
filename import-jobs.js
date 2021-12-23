b40ec182c5344d86898472c3dea52d9496d1f844
Mike Bifulco <mbifulco@live.com>
2021-12-23 11:18:24 -0500

    (hack) dynamic jobs start with d-

--- import-jobs.js
+++ import-jobs.js
@@ -60,17 +60,13 @@ ${yaml.dump(job.frontmatter)}
 ---
 ${turndownService.turndown(job.description)}`;
 
-    const newFilename = `${slugify(
-      job.frontmatter.title +
-        ' ' +
-        job.frontmatter.company +
-        ' ' +
-        job.frontmatter.id,
-      {
-        remove: /[*+~.()\/'"?!:@,]/g,
-        lower: true,
-      }
-    )}.mdx`;
+    // arbitrarily putting d- in front of job titles for dynamically loaded jobs
+    // this will be ignored in gitignore
+    const fileNameTemplate = `d-${job.frontmatter.title}-${job.frontmatter.company}-${job.frontmatter.id}`;
+    const newFilename = `${slugify(fileNameTemplate, {
+      remove: /[*+~.()\/'"?!:@,]/g,
+      lower: true,
+    })}.mdx`;
 
     const filepath = `${jobsFolder}/${newFilename}`;
 
