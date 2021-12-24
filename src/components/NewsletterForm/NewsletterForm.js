81fd99003cc98f5cf1b08f90901b6a9e6d6aff11
Mike Bifulco <mbifulco@live.com>
2021-12-24 17:38:42 -0500

    update newsletter form

--- src/components/NewsletterForm/NewsletterForm.js
+++ src/components/NewsletterForm/NewsletterForm.js
@@ -8,21 +8,19 @@ const NewsletterForm = () => {
       <p>Get Pragmatic API, HTTP And REST Info Monthly!</p>
 
       <iframe
-        title="Subscribe to our Newsletter"
         className="mj-w-res-iframe"
         frameBorder="0"
         scrolling="no"
         marginHeight="0"
         marginWidth="0"
-        height="200px"
-        src="https://app.mailjet.com/widget/iframe/5dss/qTS"
+        src="https://app.mailjet.com/widget/iframe/5dss/LBf"
         width="100%"
       ></iframe>
 
       <script
+        async
         type="text/javascript"
         src="https://app.mailjet.com/statics/js/iframeResizer.min.js"
-        async
       ></script>
     </div>
   );
