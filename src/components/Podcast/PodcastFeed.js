163ba492e9d1bc2b1fa097111ecd49625f39569d
Mike Bifulco <mbifulco@live.com>
2021-12-25 17:10:05 -0500

    support dark feed

--- src/components/Podcast/PodcastFeed.js
+++ src/components/Podcast/PodcastFeed.js
@@ -1,11 +1,13 @@
-const PodcastFeed = () => (
+const PodcastFeed = ({ dark = false }) => (
   <iframe
     width="100%"
     height="390"
     frameBorder="no"
     scrolling="no"
     seamless
-    src="https://share.transistor.fm/e/apis-you-wont-hate/playlist"
+    src={`https://share.transistor.fm/e/apis-you-wont-hate/playlist${
+      dark ? '/dark' : ''
+    }`}
   />
 );
 
