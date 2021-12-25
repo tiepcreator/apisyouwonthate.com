be9fe5fbe0eeda92ba1081a6ec7a72a7d189d9f9
Mike Bifulco <mbifulco@live.com>
2021-12-25 10:12:48 -0500

    clean up 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
-import { Container, Heading, Text } from '@chakra-ui/react';
+import { Container, Grid, Heading, Stack, Text } from '@chakra-ui/react';
+import Image from 'next/image';
 
 import { Layout, Seo } from '../components';
 
 const NotFoundPage = () => (
   <Layout>
+    <Seo title="404: Not found" />
     <Container>
-      <Seo title="404: Not found" />
-      <Heading as="h1">404: Not Found</Heading>
-      <Text>Damn, sorry. This page doesn&apos;t exist.</Text>
+      <Stack>
+        <Heading as="h1">404: Not Found</Heading>
+        <Text>
+          Damn, sorry. Either we haven&apos;t built it yet, or this page
+          doesn&apos;t exist.
+        </Text>
+      </Stack>
     </Container>
   </Layout>
 );
