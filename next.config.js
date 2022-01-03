a6fc5fde539532d464d259ab7bfea6cdca20d9b7
Mike Bifulco <mbifulco@live.com>
2022-01-03 17:05:02 -0500

    add lang attribute to html tag, and a note

--- next.config.js
+++ next.config.js
@@ -15,6 +15,27 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer(
   withMDX({
+    /* NOTE: you may be here to add localization settings that look something like what's below.
+        these work well out of the box for next.js, but they cause next-sitemap to generate a sitemap
+        which contains urls that look like apisyouwonthate.com/en-us/:splat in addition to URLs without
+        locale stuck in the middle. This has had a negative effect on SEO because our canonical links
+        do _not_ currently include /en-us/ in them. If this setting is turned on in the future, we will need
+        to update logic for generating canonical links, and possible set up 301 redirects for the old (current)
+        canonical URLs. 
+        
+        For now, to avoid rendering a production site without a  "lang" attribute in the <html> tag, we are
+        implementing /pages/_document.js per https://nextjs.org/docs/advanced-features/custom-document
+    */
+    // i18n: {
+    //   locales: ['en-US'],
+    //   defaultLocale: 'en-US',
+    //   domains: [
+    //     {
+    //       domain: 'apisyouwonthate.com',
+    //       defaultLocale: 'en-US',
+    //     },
+    //   ],
+    // },
     pageExtensions: ['js', 'jsx', 'md', 'mdx'],
     images: {
       domains: ['i.ytimg.com'],
