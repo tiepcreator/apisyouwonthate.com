b9985d3913532e9c0cb099ae691c088b9392c84b
tiepcreator <96404465+tiepcreator@users.noreply.github.com>
2022-05-12 14:36:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const siteUrl = `https://apisyouwonthate.com`;
+const siteUrl = `https://nonpin.com`;
 
 const config = {
   siteMetadata: {
-    title: `APIs You Won't Hate`,
-    description: `Articles, books, and podcasts for API Developers. Learn from the biggest community of API developers in the world.`,
+    title: `NonPin APIs You Won't Hate`,
+    description: `Nonpin Articles, books, and podcasts for API Developers. Learn from the biggest community of API developers in the world.`,
     author: `@apisyouwonthate`,
     siteUrl,
   },
